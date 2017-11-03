94825d457d8b6b752a334bec35a73bf5821e8223
yqduan0818@gmail.com <94dyq1808>
2017-11-02 17:20:56 -0700

    Fragment

---
+++ app/src/main/java/com/betteru/ucsd/myapplication4/RankingFragment.java
@@ -0,0 +1,24 @@
+package com.betteru.ucsd.myapplication4;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class RankingFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ranking, container, false);
+    }
+
+    public static RankingFragment newInstance() {
+        RankingFragment fragment = new RankingFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
