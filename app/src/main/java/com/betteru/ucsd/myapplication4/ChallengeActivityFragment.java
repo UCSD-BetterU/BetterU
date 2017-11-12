9d731b117dd8dcb70a2deb2f4a1bb162359455cc
YutingWang <wytthuss@126.com>
2017-11-12 11:45:08 -0800

    challenge add list

--- app/src/main/java/com/betteru/ucsd/myapplication4/ChallengeActivityFragment.java
+++ app/src/main/java/com/betteru/ucsd/myapplication4/ChallengeActivityFragment.java
@@ -1,9 +1,11 @@
 package com.betteru.ucsd.myapplication4;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.app.FragmentManager;
@@ -17,6 +19,7 @@ import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 
@@ -25,9 +28,13 @@ import java.time.LocalDate;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ChallengeActivityFragment extends Fragment implements DatePickerDialog.OnDateSetListener {
+
+
+public class ChallengeActivityFragment extends Fragment
+        implements DatePickerDialog.OnDateSetListener{
     static ChallengeModel data;
     View view;
+    public final static int EDITDIALOG_FRAGMENT = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,6 +52,7 @@ public class ChallengeActivityFragment extends Fragment implements DatePickerDia
         loadChallengeNameButton();
         return view;
     }
+
     public void loadChallengeName() {
         TextView name = (TextView) view.findViewById(R.id.textView_challengeName);
         name.setText(data.title);
@@ -87,13 +95,13 @@ public class ChallengeActivityFragment extends Fragment implements DatePickerDia
         });
     }
 
-    private void loadChallengeNameButton(){
+    public void loadChallengeNameButton(){
         ImageButton button = (ImageButton) view.findViewById(R.id.imageButton_challengeName);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                DialogFragment newFragment = new EditChallengeNameDialogFragment();
+                DialogFragment newFragment = EditChallengeNameDialogFragment.newInstance(data.title);
+                newFragment.setTargetFragment(ChallengeActivityFragment.this, EDITDIALOG_FRAGMENT);
                 newFragment.show(getFragmentManager(), "challengeNameEditDialog");
-                loadChallengeName();
             }
         });
     }
@@ -102,6 +110,21 @@ public class ChallengeActivityFragment extends Fragment implements DatePickerDia
     public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
         data.date = LocalDate.of(year, monthOfYear+1, dayOfMonth);
         loadChallengeDate();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case EDITDIALOG_FRAGMENT:
+                if (resultCode == Activity.RESULT_OK) {
+                    Bundle bundle = data.getExtras();
+                    String title = bundle.getString("challengeName");
+                    this.data.title = title;
+                    loadChallengeName();
+                } else if (resultCode == Activity.RESULT_CANCELED) {
+                }
+                break;
+        }
     }
 
     private class PickerAdapter extends FragmentPagerAdapter {
@@ -124,32 +147,45 @@ public class ChallengeActivityFragment extends Fragment implements DatePickerDia
         }
     }
 
-    public static class EditChallengeNameDialogFragment extends DialogFragment {
-        public EditChallengeNameDialogFragment(){}
+    public static class EditChallengeNameDialogFragment extends DialogFragment
+            implements DialogInterface.OnDismissListener{
+
+        public static EditChallengeNameDialogFragment newInstance(String title) {
+            EditChallengeNameDialogFragment f = new EditChallengeNameDialogFragment();
+            // Supply num input as an argument.
+            Bundle args = new Bundle();
+            args.putString("challengeName", title);
+            f.setArguments(args);
+            return f;
+        }
+
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the Builder class for convenient dialog construction
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             LayoutInflater inflater = getActivity().getLayoutInflater();
 
             builder.setTitle("Challenge Name");
-            builder.setView(inflater.inflate(R.layout.dialog_challenge_name, null));
-            EditText edit = (EditText) getView().findViewById(R.id.editText_challenge_name);
-            edit.setText(data.title);
+            final View dialogView = inflater.inflate(R.layout.dialog_challenge_name, null);
+            builder.setView(dialogView);
+            EditText edit = (EditText) dialogView.findViewById(R.id.editText_challenge_name);
+            String challengeName = getArguments().getString("challengeName");
+            edit.setText(challengeName);
             builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // FIRE ZE MISSILES!
-                            EditText edit = (EditText) getView().findViewById(R.id.editText_challenge_name);
-                            String result = edit.getText().toString();
-                            data.title = result;
-                        }
-                    });
+                public void onClick(DialogInterface dialog, int id) {
+                    EditText edit = (EditText) dialogView.findViewById(R.id.editText_challenge_name);
+                    String title = edit.getText().toString();
+                    Intent i = new Intent().putExtra("challengeName", title);
+                    getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, i);
+                    dismiss();
+                    //EditChallengeNameDialogFragment.this.loadChallengeName();
+                }
+            });
             builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            // User cancelled the dialog
-                            EditChallengeNameDialogFragment.this.getDialog().cancel();
-                        }
-                    });
+                public void onClick(DialogInterface dialog, int id) {
+                    // User cancelled the dialog
+                    EditChallengeNameDialogFragment.this.getDialog().cancel();
+                }
+            });
             // Create the AlertDialog object and return it
             return builder.create();
         }
---
+++ app/src/main/java/com/betteru/ucsd/myapplication4/ChallengeActivityListFragment.java
@@ -0,0 +1,81 @@
+package com.betteru.ucsd.myapplication4;
+
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Yuting on 11/12/2017.
+ */
+
+public class ChallengeActivityListFragment extends Fragment {
+    ChallengeModel data;
+    View view;
+    ArrayAdapter<String> adapter;
+    Button button;
+    ListView listView;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(LayoutInflater inflater, ViewGroup container,
+                         Bundle savedInstanceState) {
+        Bundle args = getArguments();
+        data = (ChallengeModel) args.getSerializable("data");
+
+        view = inflater.inflate(R.layout.fragment_challenge_activity, container, false);
+        findViewsById();
+
+        String[] list = getResources().getStringArray(R.array.challenge_activity_array);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, list);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        listView.setAdapter(adapter);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                SparseBooleanArray checked = listView.getCheckedItemPositions();
+                ArrayList<String> selectedItems = new ArrayList<String>();
+                for (int i = 0; i < checked.size(); i++) {
+                    // Item position in adapter
+                    int position = checked.keyAt(i);
+                    // Add sport if it is checked i.e.) == TRUE!
+                    if (checked.valueAt(i))
+                        selectedItems.add(adapter.getItem(position));
+                }
+                data.activities.clear();
+                for(int i = 0; i < selectedItems.size(); i++){
+                    data.activities.add(selectedItems.get(i));
+                }
+                backToDetailPage();
+            }
+        });
+    }
+
+    private void findViewsById() {
+        listView = (ListView) view.findViewById(R.id.listView_challengeActivity);
+        button = (Button) view.findViewById(R.id.button_challengeActivity);
+
+    }
+
+    public void backToDetailPage(){
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        ChallengeActivityFragment fragment = new ChallengeActivityFragment();
+
+        Bundle args = new Bundle();
+        args.putSerializable("data",data);
+        fragment.setArguments(args);
+        fragmentTransaction.replace(R.id.fragmentContent, fragment);
+        fragmentTransaction.commit();
+
+    }
+}
